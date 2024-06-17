126a0008d1efcd290ef33d959a873b7a8f42ac93
NOTZEUS299 <drshnptl137@gmail.com>
2024-06-17 12:03:37 +0530

    7

---
+++ components/Navbar/HeaderWithHero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderWithHero = ({children}: { children: React.ReactNode }) => {
+  return (
+    <div>HeaderWithHero</div>
+  )
+}
+
+export default HeaderWithHero
