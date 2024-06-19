950d7f24cabe787d59140662f41cbb527a0d37e4
Darshan Viradiya <drshnptl137@gmail.com>
2024-06-19 16:09:57 +0530

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Spline from "@splinetool/react-spline";
 import HeaderWithHero from "@/components/Navbar/HeaderWithHero";
+import CardTypeOne from "@/components/Cards/CardTypeOne/CardTypeOne";
 
 export default function Home() {
   return (
     <main>
       <HeaderWithHero>
-        <div className="text-white"></div>
+        <div id="heroSectionCardContainer"></div>
+        <CardTypeOne width="15em" height="30em" active={true} />
       </HeaderWithHero>
     </main>
   );
