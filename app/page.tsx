34bb675f276f9e8cc0d85a7e06b61b3d1d250a4b
Darshan Viradiya <drshnptl137@gmail.com>
2024-06-19 16:19:22 +0530

    19/06/24

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 import Spline from "@splinetool/react-spline";
 import HeaderWithHero from "@/components/Navbar/HeaderWithHero";
 import CardTypeOne from "@/components/Cards/CardTypeOne/CardTypeOne";
+import { Divider } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <main>
       <HeaderWithHero>
-        <div id="heroSectionCardContainer"></div>
-        <CardTypeOne width="15em" height="30em" active={true} />
+        <div
+          id="heroSectionCardContainer"
+          className="flex justify-evenly items-center w-[48em] h-auto"
+        >
+          <CardTypeOne width="15em" height="30em" active={true} />
+          <div className="w-[0.25px] h-[30em] bg-white" />
+          <CardTypeOne width="15em" height="30em" active={true} />
+          <div className="w-[0.25px] h-[30em] bg-white" />
+          <CardTypeOne width="15em" height="30em" active={true} />
+        </div>
       </HeaderWithHero>
     </main>
   );
