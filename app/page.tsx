6ef1635354148f4d0384be94c806d4ecec0b7609
Darshan Viradiya <drshnptl137@gmail.com>
2024-06-20 15:49:17 +0530

    3

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,28 @@
-import Spline from "@splinetool/react-spline";
 import HeaderWithHero from "@/components/Navbar/HeaderWithHero";
-import CardTypeOne from "@/components/Cards/CardTypeOne/CardTypeOne";
-import { Divider } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <main>
       <HeaderWithHero>
-        <div
-          id="heroSectionCardContainer"
-          className="flex justify-evenly items-center w-[48em] h-auto"
-        >
-          <CardTypeOne width="15em" height="30em" active={true} />
-          <div className="w-[0.25px] h-[30em] bg-white" />
-          <CardTypeOne width="15em" height="30em" active={true} />
-          <div className="w-[0.25px] h-[30em] bg-white" />
-          <CardTypeOne width="15em" height="30em" active={true} />
+        <div>
+          <div
+            id="heroSectionCardContainer"
+            className="flex justify-evenly items-center w-[33em] h-auto"
+          >
+            <div
+              id="parent-card"
+              className="w-[10em] h-[20em] rounded-large bg-black"
+            ></div>
+            <div
+              id="parent-card"
+              className="w-[10em] h-[20em] rounded-large bg-black"
+            ></div>
+            <div
+              id="parent-card"
+              className="w-[10em] h-[20em] rounded-large bg-black"
+            ></div>
+          </div>
+          <h2 className="text-white text-[3.25em] text-center leading-[1.2em] font-montserrat drop-shadow-2xl" id="routine-simplified">Routine Simplified</h2>
         </div>
       </HeaderWithHero>
     </main>
