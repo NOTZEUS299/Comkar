493554d7d459ecfb3366604285ccffeab73b8c4b
Darshan Viradiya <drshnptl137@gmail.com>
2024-06-24 17:27:20 +0530

    2

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,7 @@ export default function Home() {
   return (
     <main>
       <HeaderWithHero>
-        <div>
-          <div
-            id="heroSectionCardContainer"
-            className="flex justify-evenly items-center w-[33em] h-auto"
-          >
-            <div
-              id="parent-card"
-              className="w-[10em] h-[20em] rounded-large bg-black"
-            ></div>
-            <div
-              id="parent-card"
-              className="w-[10em] h-[20em] rounded-large bg-black"
-            ></div>
-            <div
-              id="parent-card"
-              className="w-[10em] h-[20em] rounded-large bg-black"
-            ></div>
-          </div>
-          <h2 className="text-white text-[3.25em] text-center leading-[1.2em] font-montserrat drop-shadow-2xl" id="routine-simplified">Routine Simplified</h2>
-        </div>
+        <></>
       </HeaderWithHero>
     </main>
   );
